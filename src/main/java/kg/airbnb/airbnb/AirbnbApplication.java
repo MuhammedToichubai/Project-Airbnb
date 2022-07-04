a234d80697d3afd5264f796b6b58c9eb00aa19d8
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-07-04 18:14:12 +0600

    Create empty project

---
+++ src/main/java/kg/airbnb/airbnb/AirbnbApplication.java
@@ -0,0 +1,23 @@
+package kg.airbnb.airbnb;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@SpringBootApplication
+public class AirbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbApplication.class, args);
+		System.out.println("Welcome colleagues, project name is Airbnb!");
+	}
+
+	@GetMapping("/")
+	public String greetingPage(){
+		return "<h1>Welcome to Airbnb application!!!<h1/>";
+	}
+}
