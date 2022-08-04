0518f25d69cd1b67d76eb0ce771aecdbfe3de3c6
Muhammed <muhammedtoichubai@gmail.com>
2022-08-04 06:24:36 -0600

    admin and user saved by default

---
+++ src/main/resources/data-initial/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, full_name, email, password, image, role)
+VALUES (2,'Admin', 'airbnb@mail.com', '$2a$10$UG2qe9cH4iB00B9dZvLp0exUt5WyQrN/eMOboNE3.tHV4hFXD9lr2',
+        'https://thumbs.dreamstime.com/b/admin-sign-laptop-icon-stock-vector-166205404.jpg', 'ADMIN'),
+       (3,'Bill Gates', 'billgates@mail.com', '$2a$10$.YQm2c3IhgaqYVzeh3REFeMpCswkZ99bFznWwNLG7l0TRmji/YvRi',
+        'https://thumbs.dreamstime.com/b/admin-sign-laptop-icon-stock-vector-166205404.jpg', 'USER');
