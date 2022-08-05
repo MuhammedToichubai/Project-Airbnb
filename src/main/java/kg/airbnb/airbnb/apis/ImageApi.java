7dd9a042522bea5e16e38b2841827e5f2f2b8372
JkpbK <6jakyp@gmail.com>
2022-08-05 19:08:28 +0600

    s3 file upload

---
+++ src/main/java/kg/airbnb/airbnb/apis/ImageApi.java
@@ -0,0 +1,29 @@
+package kg.airbnb.airbnb.apis;
+
+import kg.airbnb.airbnb.services.impl.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/file")
+public class ImageApi {
+
+    private final ImageService service;
+
+    @PostMapping(value = {"/upload"},
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
