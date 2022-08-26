a4fe262e922f1bc5fe5a85cc0899f8056e0bb7a7
Muhammed <muhammedtoichubai@gmail.com>
2022-08-26 05:21:35 -0600

    added service class and API for google map

---
+++ src/main/java/kg/airbnb/airbnb/apis/GoogleMapApi.java
@@ -0,0 +1,23 @@
+package kg.airbnb.airbnb.apis;
+
+import kg.airbnb.airbnb.services.googlemap.GoogleMapService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+public class GoogleMapApi {
+
+    private final GoogleMapService googleMapService;
+
+    @GetMapping("/find/place")
+    Map<String, String> findPlaceByLatAndLng(@RequestParam String lat,
+                                             @RequestParam String lng){
+     return Map.of("place", googleMapService.findPlace(lat, lng));
+    }
+
+}
---
+++ src/main/java/kg/airbnb/airbnb/services/googlemap/GoogleMapService.java
@@ -0,0 +1,38 @@
+package kg.airbnb.airbnb.services.googlemap;
+
+import kg.airbnb.airbnb.repositories.AnnouncementRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Getter
+@Setter
+public class GoogleMapService {
+
+    private final RestTemplate restTemplate;
+    private final AnnouncementRepository announcementRepository;
+
+    @Value("${google.api-key}")
+    private String googleApikey;
+
+
+    public GoogleMapService(AnnouncementRepository announcementRepository) {
+        this.announcementRepository = announcementRepository;
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String findPlace(String lat, String lng){
+        Object object = restTemplate.getForObject(
+                String.format("%s?latlng=%s, %s&key=%s",
+                        "https://maps.googleapis.com/maps/api/geocode/json",
+                        lat,
+                        lng,
+                        googleApikey),
+                Object.class
+        );
+        return object.toString();
+    }
+}
