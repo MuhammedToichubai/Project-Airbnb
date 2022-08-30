c1c7f8eb95e8e28575c386bdf2ec22f1876a0e97
Muhammed <muhammedtoichubai@gmail.com>
2022-08-30 07:03:58 -0600

    fixed class UserProfileMapper

--- src/main/java/kg/airbnb/airbnb/mappers/user/UserProfileViewMapper.java
+++ src/main/java/kg/airbnb/airbnb/mappers/user/UserProfileViewMapper.java
@@ -1,4 +1,4 @@
-package kg.airbnb.airbnb.mappers;
+package kg.airbnb.airbnb.mappers.user;
 
 
 import kg.airbnb.airbnb.dto.responses.UserAnnouncementResponse;
@@ -30,6 +30,7 @@ public class UserProfileViewMapper {
             return null;
         }
         UserAnnouncementResponse announcementsResponse = new UserAnnouncementResponse();
+        announcementsResponse.setId(announcement.getId());
         announcementsResponse.setImage(announcement.getImages().get(0));
         announcementsResponse.setPrice(announcement.getPrice());
         announcementsResponse.setRating(announcementViewMapper.calculateRating(announcement));
