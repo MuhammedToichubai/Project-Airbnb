f05c2bdb7e0f741bea7f58620cf2f44516d3a5ab
Muhammed <muhammedtoichubai@gmail.com>
2022-08-30 05:55:04 -0600

    bugfix announcement find by id

--- src/main/java/kg/airbnb/airbnb/dto/responses/AnnouncementResponse.java
+++ src/main/java/kg/airbnb/airbnb/dto/responses/AnnouncementResponse.java
@@ -1,7 +1,9 @@
 package kg.airbnb.airbnb.dto.responses;
 
 import kg.airbnb.airbnb.enums.Type;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -9,23 +11,18 @@ import java.util.List;
 
 @Getter
 @Setter
-public class AnnouncementInnerPageResponse {
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnnouncementResponse {
     private Long id;
     private List<String> images;
     private Type houseType;
     private Integer maxGuests;
     private BigDecimal price;
     private String title;
-    private String location;
     private String description;
-    private Long userID;
-    private String ownerImage;
-    private String ownerFullName;
-    private String ownerEmail;
-    private Integer likeCount;
-    private Integer bookmarkCount;
-    private Integer viewAnnouncementCount;
-    private String colorOfLike;
-    private String colorOfBookmark;
-
+    private Long regionId;
+    private String RegionName;
+    private String townProvince;
+    private String address;
 }
