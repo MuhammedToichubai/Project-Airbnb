cbedfb40e1a5fb738fe3228a618775519a560a54
Muhammed <muhammedtoichubai@gmail.com>
2022-09-09 06:26:41 -0600

    delete application-test.yml

--- src/test/java/kg/airbnb/airbnb/AirbnbApplicationTests.java
+++ src/test/java/kg/airbnb/airbnb/AirbnbApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AirbnbApplicationTests {
+public class AirbnbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads(){
 
+    }
 }
