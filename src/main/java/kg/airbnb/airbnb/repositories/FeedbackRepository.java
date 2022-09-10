6225744caf2595ef47595fd4e9ebe48a9718d64c
Beksultan <90905002+beksultancs@users.noreply.github.com>
2022-09-10 22:05:35 +0600

    Update FeedbackRepository.java

--- src/main/java/kg/airbnb/airbnb/repositories/FeedbackRepository.java
+++ src/main/java/kg/airbnb/airbnb/repositories/FeedbackRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long>, PagingAndSortingRepository<Feedback,Long> {
 
     @Query(value = "select distinct f from Feedback f where f.announcement.id = ?1")
-    List<Feedback> findAnnouncementFeedback(@Param("announcementId")Long announcementId, Pageable pageable);
+    List<Feedback> findAnnouncementFeedback(Long announcementId, Pageable pageable);
 }
