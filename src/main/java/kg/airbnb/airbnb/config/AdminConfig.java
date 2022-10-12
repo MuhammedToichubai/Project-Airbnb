27653ba23d4f03afc2f929dd8879a1a5214a219f
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-12 15:07:42 +0600

    refactor v14

--- src/main/java/kg/airbnb/airbnb/config/AdminConfig.java
+++ src/main/java/kg/airbnb/airbnb/config/AdminConfig.java
@@ -1,31 +1,31 @@
-package kg.airbnb.airbnb.config;
-
-import kg.airbnb.airbnb.enums.Role;
-import kg.airbnb.airbnb.models.auth.User;
-import kg.airbnb.airbnb.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@RequiredArgsConstructor
-public class AdminConfig {
-
-    private final PasswordEncoder passwordEncoder;
-
-    @Bean
-    CommandLineRunner commandLineRunner(UserRepository userRepository) {
-        return args -> {
-            String password = "admin";
-            User user = new User(
-                    "Airbnb Admin",
-                    "admin@gmail.com",
-                    passwordEncoder.encode(password),
-                    Role.ADMIN
-            );
-            userRepository.save(user);
-        };
-    }
-}
+//package kg.airbnb.airbnb.config;
+//
+//import kg.airbnb.airbnb.enums.Role;
+//import kg.airbnb.airbnb.models.auth.User;
+//import kg.airbnb.airbnb.repositories.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class AdminConfig {
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(UserRepository userRepository) {
+//        return args -> {
+//            String password = "admin";
+//            User user = new User(
+//                    "Airbnb Admin",
+//                    "admin@gmail.com",
+//                    passwordEncoder.encode(password),
+//                    Role.ADMIN
+//            );
+//            userRepository.save(user);
+//        };
+//    }
+//}
