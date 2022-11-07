2b4b75c54a87ff4bbea494bc33466197eb702bcc
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-07 17:37:13 +0600

    refactor AuthApi

--- src/main/java/kg/airbnb/airbnb/apis/AuthApi.java
+++ src/main/java/kg/airbnb/airbnb/apis/AuthApi.java
@@ -10,40 +10,47 @@ import kg.airbnb.airbnb.dto.requests.UserRegisterRequest;
 import kg.airbnb.airbnb.dto.responses.SimpleResponse;
 import kg.airbnb.airbnb.security.LoginService;
 import kg.airbnb.airbnb.services.impl.AuthService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("api/public")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Authentication API", description = "Users with role\"Admin\",\"User\" can authenticate.")
+@Tag(name = "Authentication API", description = "Registration and authentication")
 public class AuthApi {
 
     private final AuthService authService;
     private final LoginService loginService;
 
-    @Operation(summary = "Registration", description = "Any user can register.")
-    @PostMapping("/user/register")
+    @Operation(summary = "Registration", description = "Any user can register")
+    @PostMapping("register")
     public JwtResponse registrationPerson(@RequestBody UserRegisterRequest userRegisterRequest) {
         return authService.registerUser(userRegisterRequest);
     }
 
-    @Operation(summary = "Sign in", description = "Any registered user can login.")
-    @PostMapping("/login")
+    @Operation(summary = "Sign in", description = "Any registered user can login")
+    @PostMapping("login")
     public JwtResponse performLogin(@RequestBody LoginRequest loginResponse) {
         return loginService.authenticate(loginResponse);
     }
 
-    @Operation(summary = "Sign in with Google", description = "Any user can login with google account.")
-    @PostMapping("/login/google")
+    @Operation(summary = "Sign in with Google", description = "Any user can login with google account")
+    @PostMapping("auth-google")
     public JwtResponse loginWithGoogle(@RequestParam String token) throws FirebaseAuthException {
         return loginService.authenticateWithGoogle(token);
     }
 
     @Operation(summary = "Add phone number", description = "Registered user must add phone number")
-    @PostMapping("/user/add/phone/number")
-    public SimpleResponse addPhoneNumber(@RequestBody PhoneNumberRequest phoneNumberRequest) {
-        return loginService.addPhoneNumber(phoneNumberRequest);
+    @PostMapping("phone-number")
+    public SimpleResponse addPhoneNumber(@RequestBody PhoneNumberRequest request) {
+        return loginService.addPhoneNumber(request);
     }
+
 }
