06b291750989dd5a0df25e2456212b687ec73823
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-08 16:37:22 +0600

    refactor Booking entity

--- src/main/java/kg/airbnb/airbnb/model/Booking.java
+++ src/main/java/kg/airbnb/airbnb/model/Booking.java
@@ -5,19 +5,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static javax.persistence.CascadeType.*;
+import static javax.persistence.CascadeType.DETACH;
+import static javax.persistence.CascadeType.MERGE;
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.CascadeType.REFRESH;
 import static javax.persistence.FetchType.EAGER;
 
-@Entity
-@Table(name = "bookings")
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "booking_id_generator")
     @SequenceGenerator(name = "booking_id_generator", sequenceName = "booking_seq", allocationSize = 1, initialValue = 28)
@@ -38,4 +48,5 @@ public class Booking {
     private Status status;
 
     private LocalDate createdAt;
+
 }
