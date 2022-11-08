47e6edaf2be1a6bd96cd48ad89f397fb11dc062e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-08 17:06:31 +0600

    refactor UserRepository

--- src/main/java/kg/airbnb/airbnb/repositories/UserRepository.java
+++ src/main/java/kg/airbnb/airbnb/repositories/UserRepository.java
@@ -15,32 +15,32 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
 
-    @Query("select u from User u where  u.role = 'USER' ")
+    @Query("SELECT u FROM User u WHERE  u.role = 'USER'")
     List<User> findAllUsers();
 
     @Modifying
     @Transactional
-    @Query(value = "delete from user_messages_from_admin m where m.user_id = :userId", nativeQuery = true)
+    @Query(value = "DELETE FROM user_messages_from_admin m WHERE m.user_id = :userId", nativeQuery = true)
     void clearMessages(Long userId);
 
     @Modifying
     @Transactional
-    @Query("delete from User u where u.id = ?1")
+    @Query("DELETE FROM User u WHERE u.id = ?1")
     void customDeleteById(Long userId);
 
     @Modifying
     @Transactional
-    @Query(value = "delete from feedbacks f where f.owner_id = ?1", nativeQuery = true)
+    @Query(value = "DELETE FROM feedbacks f WHERE f.owner_id = ?1", nativeQuery = true)
     void clearFeedbacks(Long userId);
 
     @Modifying
     @Transactional
-    @Query(value = "delete from bookings b where b.user_id = :userId", nativeQuery = true)
+    @Query(value = "DELETE FROM bookings b WHERE b.user_id = :userId", nativeQuery = true)
     void clearBookings(Long userId);
 
     @Modifying
     @Transactional
-    @Query(value = "delete from announcements a where a.owner_id = :userId ", nativeQuery = true)
+    @Query(value = "DELETE FROM announcements a WHERE a.owner_id = :userId ", nativeQuery = true)
     void clearAnnouncements(Long userId);
 
 }
