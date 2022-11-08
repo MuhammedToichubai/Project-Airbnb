36a8df216c9720dafe93046474ee05bdaf8c22ed
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-08 17:13:59 +0600

    refactor GoogleMapService

--- src/main/java/kg/airbnb/airbnb/services/GoogleMapService.java
+++ src/main/java/kg/airbnb/airbnb/services/GoogleMapService.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Service
 @Getter
 @Setter
+@Service
 public class GoogleMapService {
 
     private final RestTemplate restTemplate;
