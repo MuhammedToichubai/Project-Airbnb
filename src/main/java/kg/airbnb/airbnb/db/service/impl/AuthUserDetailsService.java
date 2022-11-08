758cc6594f5991245e41e0c4d40714c638c1dd3e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-08 17:31:46 +0600

    refactor AuthUserDetailsService

--- src/main/java/kg/airbnb/airbnb/db/service/impl/AuthUserDetailsService.java
+++ src/main/java/kg/airbnb/airbnb/db/service/impl/AuthUserDetailsService.java
@@ -4,6 +4,7 @@ import kg.airbnb.airbnb.db.repositories.UserRepository;
 import kg.airbnb.airbnb.exceptions.NotFoundException;
 import kg.airbnb.airbnb.db.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,7 @@ public class AuthUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByEmail(email);
         if (user.isEmpty()) {
             throw new NotFoundException("User with " + email + " not found!");
